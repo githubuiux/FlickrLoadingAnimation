0ca1b99d657f197242d837df3c8746381c7b25d3
githubuiux <uiuxista@gmail.com>
2023-04-02 19:10:18 +0300

    Initial Commit

---
+++ FlickrLoadingAnimation/FlickrLoadingAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlickrLoadingAnimationApp.swift
+//  FlickrLoadingAnimation
+//
+//  Created by Levent on 2.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct FlickrLoadingAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
